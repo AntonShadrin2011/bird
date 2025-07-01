0fb33e0a16957970a93a2fee85c57d1c9ae7e2e2
Anton.Shadrin <a.v.shadrin@inbox.ru>
2025-07-01 19:55:41 +0300

    1

--- птички/bird.py
+++ птички/bird.py
@@ -6,7 +6,11 @@ SCREEN_HEIGHT = 800
 SCREEN_TITLE = 'Bird'
 
 class Bird(arcade.Sprite):
-    def fff(self):
+    def __init__(self):
+        super().__init__('bird.png', scale=1)
+
+    def logika(self):
+        self.center_y += self.change_y
         self.change_y -= 1
         if self.bottom < 250:
             self.bottom = 250
@@ -22,18 +26,19 @@ class Bird(arcade.Sprite):
 
 class Truba(arcade.Sprite):
     def __init__(self):
-        super().__init__(os.path.join('/Users/anton/PycharmProjects/PythonProject/птички', 'pipe.png'), scale=1)
-
-    def logika(self):
-        self.center_x -= 2
-
+        super().__init__('pipe.png', scale=0.3)
+        self.change_x = 7
+    def update(self):
+        self.center_x -= self.change_x
+        if self.center_x < 3:
+            self.center_x = 1350
+            self.zapret = False
 
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.bird = Bird()
-        self.background = arcade.load_texture(
-            os.path.join('/Users/anton/PycharmProjects/PythonProject/птички', 'bg.png'))
+        self.background = arcade.load_texture('bg.png')
         self.truby = arcade.SpriteList()
         self.score = 0
 
@@ -42,8 +47,8 @@ class MyGame(arcade.Window):
         self.bird.center_x = 237
         for i in range(4):
             truba = Truba()
-            truba.center_x = 800 + i * 300
-            truba.center_y = 250
+            truba.center_x = 230 * i
+            truba.center_y = 177
             self.truby.append(truba)
 
     def on_draw(self):
@@ -57,10 +62,7 @@ class MyGame(arcade.Window):
     def update(self, delta_time):
         self.bird.logika()
         self.truby.update()
-        for tube in self.truby:
-            if tube.right < self.bird.left and not hasattr(tube, 'passed'):
-                tube.passed = True
-                self.score += 1
+
 
     def on_key_press(self, key, mods):
         if key == arcade.key.SPACE:
